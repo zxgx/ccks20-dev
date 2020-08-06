83062fc27e8486c9b190a30821f87989b2bcf5a6
zxgx <zhangg1998@outlook.com>
2020-08-06 16:36:35 +0800

    load_corpus

---
+++ helper/LoadCorpus.py
@@ -0,0 +1,70 @@
+import re
+import json
+
+
+def load_corpus(path):
+    corpus = []
+    question_num = 0
+    e1hop1_num, e1hop2_num, e2hop2_num = 0, 0, 0
+    with open(path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for i in range(0, len(lines), 4):
+            # 问题
+            line = lines[i].strip().split(':')
+            idx = line[0]
+            question = ':'.join(line[1:]) # 问题中含有多个冒号
+            question = re.sub('你了解', '', question)
+            question = re.sub('我想知道', '', question)
+            question = re.sub('请问', '', question)
+            
+            # 答案
+            answer = lines[i+2].strip().split('\t')
+            
+            # 查询，包含生成答案的正确路径
+            sparql = lines[i+1].strip()
+            where = re.findall('{.+}', sparql)[0]
+            elements = re.findall('<.+?>|\".+?\"|\?\D', where) # 没有再加双引号项
+            
+            gold_entities, gold_relations = [], []
+            for j in range(len(elements)):
+                if elements[j][0] == '<' or elements[j][0] == '"':
+                    if j%3 == 1:
+                        gold_relations.append(elements[j])
+                    else:
+                        gold_entities.append(elements[j])
+            gold_tuple = tuple(gold_entities+gold_relations)
+            corpus.append({
+                'id': idx,
+                'question': question,
+                'answer': answer,
+                'gold_entities': gold_entities,
+                'gold_relations': gold_relations,
+                'sparql': sparql
+            })
+            
+            if len(gold_entities) == 1 and len(gold_relations) == 1:
+                e1hop1_num += 1
+            elif len(gold_entities) == 1 and len(gold_relations) == 2:
+                e1hop2_num += 1
+            elif len(gold_entities) == 2 and len(gold_relations) == 2:
+                e2hop2_num += 1
+            elif len(gold_entities) == 2 and len(gold_relations) < 2:
+                print(idx)
+                print (elements)
+                print (gold_entities)
+                print (sparql)
+                print ('\n')
+            question_num += 1
+    
+    print("问题数为%d，单实体单关系为%d，单实体双关系为%d，双实体双关系为%d，总比例为%.4f\n"%
+        (question_num, e1hop1_num, e1hop2_num, e2hop2_num, 
+        (e1hop1_num+e1hop2_num+e2hop2_num)/question_num))
+    return corpus
+
+
+if __name__ == '__main__':
+    corpus_path = '../corpus/task1-4_train_2020.txt'
+    data_path = '../data/corpus_train.json'
+    corpus = load_corpus(corpus_path)
+    with open(data_path, 'w', encoding='utf-8') as f:
+        json.dump(corpus, f, ensure_ascii=False, indent=4)
---
+++ main.py
@@ -0,0 +1,69 @@
+import json
+from GstoreConnector import GstoreConnector
+
+
+DB_HOST = "pkubase.gstore.cn"
+DB_NAME = "pkubase"
+PORT = 80
+USER_NAME = "endpoint"
+PASSWORD = "123"
+
+
+def load_data(path):
+    data = []
+    with open(path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for i in range(0, len(lines), 4):
+            q = lines[i].split(':')
+            '''
+            if len(q) != 2:
+                print(i, q)
+                continue
+            '''
+            q_id = q[0]
+            q_text = ':'.join(q[1:]).strip() # 或许会有问题
+            sparql = lines[i+1].strip()
+            ans = lines[i+2].strip().split('\t')
+            data.append({
+                'id':q_id, 'question': q_text, 'sparql': sparql, 'ans': ans
+            })
+    return data
+    
+
+if __name__ == '__main__':
+    dataset_path = 'dataset/task1-4_train_2020.txt'
+    dataset = load_data(dataset_path)
+    
+    idx = [357, 6288//4, 9844//4, 10084//4] # 后3个问题里含有‘:’
+    for i in idx:
+        print(dataset[i]['question'])
+    
+    print('\n'+'*'*50+'\n')
+    
+    gcon = GstoreConnector(DB_HOST, PORT, USER_NAME, PASSWORD)
+    item = dataset[207]
+    ret = json.loads(gcon.query(DB_NAME, 'json', item['sparql']))   # 编码问题
+    print(ret)
+    print('\n'+'*'*50+'\n')
+    
+    for i, item in enumerate(dataset):
+        # 查询是一个json字符串
+        # https://www.w3.org/TR/sparql11-overview/#sparql11-results
+        ret = json.loads(gcon.query(DB_NAME, 'json', item['sparql'])) 
+        
+        if len(ret['head']['vars']) != 1: # 数据中含有多个变量的问题
+            print(str(i)+'\n', item, '\n')
+            continue
+
+        var, values = ret['head']['vars'][0], []
+        for v in ret['results']['bindings']:
+            if v[var]['type'] == 'uri':
+                values.append('<'+v[var]['value']+'>')
+            else:
+                values.append('"'+v[var]['value']+'"')
+        for v in values:
+            if v not in item['ans']:
+                print(i)
+                print(values)
+                print(item['ans'], '\n')
+                break
